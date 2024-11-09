8bd9d70c97ce445d66b0a0323ff81fb449a38bb0
Johnatan Coelho <johnatansouza242@gmail.com>
2024-11-09 15:23:45 -0300

    Arrumando bug

--- javascript.js
+++ javascript.js
@@ -4,26 +4,6 @@ class heroi{
             this.idadeHeroi = idadeHeroi;
             this.tipoHeroi = tipoHeroi;
     }
-    tipo() {
-        let ataque
-        if(this.tipoHeroi === "Mago"){
-             ataque = "Magia"
-            console.log(`o ${this.tipoHeroi} atacou usando ${ataque}`)
-        }
-        else if(this.tipoHeroi === "Guerreiro"){
-             ataque = "Espada"
-            console.log(`o ${this.tipoHeroi} atacou usando ${ataque}`)  
-        }
-        else if(this.tipoHeroi === "Monge"){
-            ataque = "Artes Marciais"
-           console.log(`o ${this.tipoHeroi} atacou usando ${ataque}`)  
-       }
-       else if(this.tipoHeroi === "Ninja"){
-        ataque = "Shuriken"
-       console.log(`o ${this.tipoHeroi} atacou usando ${ataque}`)  
-   }
-        
-    }
 
 }
 
